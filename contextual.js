b5f0cd9ad81085fd20263d73840af9282005efd0
Stijn De Meyere <stijn@sken.be>
2010-06-27 01:38:48 +0200

    Fixing my tree, should be up to date now.

--- contextual.js
+++ contextual.js
@@ -1,41 +1,61 @@
 /* $Id$ */
 
-Drupal.contextual = Drupal.contextual || {};
-Drupal.contextual.overlay = '<div class="contextual-border contextual-border-top"></div><div class="contextual-border contextual-border-right"></div><div class="contextual-border contextual-border-bottom"></div><div class="contextual-border contextual-border-left"></div>';
+/*global $,Drupal */
+"use strict";
 
-Drupal.behaviors.contextual = function(context) {
-  $("div.contextual-enabled").hover(function(event) {
-    // Get the right actions from the closure region
-    var classes = ($(this).attr("class"));
-    var identifier = '#contextual-'+ classes.replace(/([^ ]+[ ]+)*contextual-([^ ]+)([ ]+[^ ]+)*/, '$2');
-    $(this).prepend($(identifier));
-    $(this).css({'position': 'relative'});
+Drupal.behaviors.contextual = function (context) {
+  $(".contextual-enabled:not(.contextual-processed)", context).each(function () {
+    var positioned = true,
+      // Get the right actions from the closure region
+      identifier = '#contextual-' + $(this).attr("class")
+        .replace(/([^ ]+[ ]+)*contextual-([^ ]+)([ ]+[^ ]+)*/, '$2'),
+      $links = $(identifier);
 
-    // hide parent actions
-    $('.contextual').css('padding-left', 100);
-    $('.contextual-links').hide();
-    $('.contextual').css('display', 'none');
-    $('.contextual-border').remove();
-    $('a.contextual-toggler').removeClass('contextual-toggler-active');
+    $('<a class="contextual-toggler">').click(function () {
+        var $menu = $('.contextual-links', $links);
+        if ($menu.is(":hidden")) {
+          $menu.slideDown('fast');
+        } else {
+          $menu.hide();
+        }
+        $(this).toggleClass('contextual-toggler-active');
+      })
+      .prependTo($('.contextual-wrapper', $links));
 
-    // Show current actions
-    $('.contextual:first', this).css('display', 'block').append(Drupal.contextual.overlay);
-  },
-  function() {
-    $('.contextual', this).css('display', 'none');
-    $('.contextual-border', this).remove();
-  });
-}
+    $(this).prepend($links);
+
+    $(this).hover(function () {
+      if ($(this).css('position') === 'static') {
+        positioned = false;
+        $(this).css({'position': 'relative'});
+      }
+
+      // hide parent actions
+      $('.contextual').css('padding-left', 100);
+      $('.contextual-links:visible').hide();
+      $('.contextual:visible').hide();
+      $('.contextual-border').remove();
+      $('a.contextual-toggler').removeClass('contextual-toggler-active');
+
+      // Show current actions
+      $links.show().append(Drupal.theme('contextualOverlay'));
+    },
+    function () {
+      if (!positioned) {
+        $(this).css('position', 'static');
+      }
+      $links.hide();
+      $('.contextual-border', this).remove();
+    });
+  })
+  .addClass('contextual-processed');
+};
 
 /**
- * toggles visibility on an element
+ * Theme function for contextual overlay.
  */
-Drupal.contextual.toggleVis = function(element) {
-  var element = $('#' + element);
-  if (element.is(":hidden")) {
-    element.slideDown('fast');
-  } else {
-    element.hide();
-  }
-  $('a.contextual-toggler', element.parent()).toggleClass('contextual-toggler-active');
-}
+Drupal.theme.prototype.contextualOverlay = function () {
+  return '<div class="contextual-border contextual-border-top"></div><div class="contextual-border contextual-border-right"></div><div class="contextual-border contextual-border-bottom"></div><div class="contextual-border contextual-border-left"></div>';
+};
+
+/*jslint white: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, newcap: true, immed: true, strict: true, indent: 2 */
